01f8d297c46bef738e956254074046819479bb95
sigurdo <36281346+sigurdo@users.noreply.github.com>
2018-04-10 21:56:09 +0200

    La til Freaking Math og daglig tubaøving

---
+++ dagligOvingTuba/js/dagligOvingTuba.js
@@ -0,0 +1,68 @@
+//Lager metronom:
+function slag() {
+	klikk.play();
+	setTimeout(slag, 60 / bpm * 1000);
+}
+
+klikk = document.querySelector('#klikk');
+
+var bpm = 0;
+
+slag();
+
+//Lager navigasjon:
+function byttOvelse(nr) {
+	if (nr >= 0 && nr < antOvelser) {
+		document.querySelector('#ovelse'+aktivOvelse).style.display = 'none';
+		aktivOvelse = nr;
+		document.querySelector('#ovelse'+aktivOvelse).style.display = 'block';
+		kjorOvelse();
+	}
+}
+
+//Nivaer til ovelse5:
+function nyeNivaer() {
+	var nivaer = document.querySelectorAll('#nivaer .niva');
+
+	for (var i = 0; i < nivaer.length; i++) {
+		var n = ssf.randInt(1, 6);
+
+		nivaer[i].innerHTML = '';
+		for (var j = 0; j < n; j++) {
+			nivaer[i].innerHTML += '<div></<div>';
+		}
+	}
+}
+
+var antOvelser = 6;
+var aktivOvelse = 0;
+kjorOvelse(0);
+
+function kjorOvelse() {
+	var nr = aktivOvelse;
+	if (nr == 0) {
+		bpm = 72;
+	}
+
+	if (nr == 1) {
+		bpm = 72;
+	}
+
+	if (nr == 2) {
+		bpm = 72;
+	}
+
+	if (nr == 3) {
+		bpm = 92;
+	}
+
+	if (nr == 4) {
+		bpm = 112;
+	}
+
+	if (nr == 5) {
+		bpm = 72;
+		nyeNivaer();
+	}
+}
+
